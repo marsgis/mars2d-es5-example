7efb97ec2b02041a0bf63ab38907c4002f71e05c
wanghao <wh@marsgis.cn>
2022-03-26 14:19:06 +0800

    同步最新代码

--- example/js/common.js
+++ example/js/common.js
@@ -1,12 +1,15 @@
 "use script"; //开发环境建议开启严格模式
 
 //读取 config.json 配置文件
-let configUrl = "config/config.json";
+let configUrl = "../config/config.json";
 
 mars2d.Util.fetchJson({ url: configUrl })
   .then(function (data) {
     //构建地图
     window.initMap(data.mars2d);
+    if (mars2d.widget && window.map) {
+      initWidget(window.map);
+    }
   })
   .catch(function (error) {
     console.log(error);
@@ -17,7 +20,7 @@ function openTipView(content, title) {
   window.layer.open({
     type: 1,
     title: title || "功能 和 已知问题 提示",
-    offset: "rt",
+    offset: "rb",
     shade: false,
     skin: "animation-scale-up",
     content: content,
@@ -26,4 +29,33 @@ function openTipView(content, title) {
 
 if ($("#tipView").length > 0) {
   openTipView($("#tipView"));
+}
+
+//初始化widget相关
+function initWidget(map) {
+  //初始化widget管理器
+  mars2d.widget.init(map, {
+    defaultOptions: {
+      windowOptions: { position: { bottom: 50, left: 10 } },
+    },
+    openAtStart: [
+      {
+        name: "右上角工具栏",
+        uri: "widgets/toolButton/menuBtn.js",
+      },
+    ],
+    widgets: [
+      {
+        name: "图层管理",
+        uri: "widgets/manageLayers/widget.js",
+        group: "forlayer",
+        autoCenter: true,
+        windowOptions: {
+          position: { top: 10, bottom: 40, left: 50 },
+        },
+        autoDisable: false,
+        disableOther: false,
+      },
+    ],
+  });
 }
